bb7e2a91603f891802c83a7f6953b2883da503de
shaul-atiya <46088795+shaul-atiya@users.noreply.github.com>
2021-05-17 10:32:12 +0300

    adding  graphql codegen
    based on https://github.com/deweyjose/graphqlcodegen

--- src/main/java/com/blossom/users/BlossomUsersManagementApiApplication.java
+++ src/main/java/com/blossom/users/BlossomUsersManagementApiApplication.java
@@ -2,12 +2,21 @@ package com.blossom.users;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
+import com.blossom.exceptions.BlossomDataFetchingExceptionHandler;
+
+
+@Import({
+	BlossomDataFetchingExceptionHandler.class
+})
 @SpringBootApplication
 public class BlossomUsersManagementApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlossomUsersManagementApiApplication.class, args);
+		
+		
 	}
 
 }
--- src/main/java/com/blossom/users/fethers/UsersDataFetcher.java
+++ src/main/java/com/blossom/users/fethers/UsersDataFetcher.java
@@ -3,22 +3,24 @@ package com.blossom.users.fethers;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import com.blossom.users.dto.User;
+import com.blossom.exceptions.BlossomException;
+import com.blossom.exceptions.BlossomExceptionType;
+import com.blossom.userManagmet.generated.types.User;
 import com.netflix.graphql.dgs.DgsComponent;
 import com.netflix.graphql.dgs.DgsQuery;
 import com.netflix.graphql.dgs.InputArgument;
 
+
 @DgsComponent
 public class UsersDataFetcher {
 
-	private final List<User> users = List.of(new User("shaul", "atiya", "shaula", 37, "shker@amel.com"),
-			new User("avi", "prize", "avipr", 35, "shker@amel.com"),
-			new User("dummy", "user", "dumdum", 31, "aa@amel.com"));
+	private final List<User> users = List.of(new User("shaul","atiya","","shaula",12,"eng","a@b.com"));
 
 	@DgsQuery
 	public List<User> users(@InputArgument String firstNameFilter) {
+		
 		if (firstNameFilter == null) {
-			return users;
+			throw new BlossomException(null,BlossomExceptionType.VALIDATION,"1","this is a test exeption");
 		}
 
 		return users.stream().filter(s -> s.getFirstName().contains(firstNameFilter)).collect(Collectors.toList());
