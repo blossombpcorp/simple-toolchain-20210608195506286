959054130790f123d9e396d416e98e7bec2cf6ac
shaul-atiya <46088795+shaul-atiya@users.noreply.github.com>
2021-05-22 12:32:28 +0300

    adding Mutation for add user

--- src/main/java/com/blossom/users/fethers/UsersDataFetcher.java
+++ src/main/java/com/blossom/users/fethers/UsersDataFetcher.java
@@ -3,13 +3,16 @@ package com.blossom.users.fethers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 
 import com.blossom.exceptions.BlossomException;
 import com.blossom.exceptions.BlossomExceptionType;
 import com.blossom.usermanagmet.generated.types.User;
+import com.blossom.usermanagmet.generated.types.UserInput;
 import com.blossom.users.mapper.UsersMapper;
 import com.blossom.users.repositories.UsersRepository;
 import com.netflix.graphql.dgs.DgsComponent;
+import com.netflix.graphql.dgs.DgsMutation;
 import com.netflix.graphql.dgs.DgsQuery;
 import com.netflix.graphql.dgs.InputArgument;
 
@@ -24,10 +27,20 @@ public class UsersDataFetcher {
 	@DgsQuery
 	public List<User> users(@InputArgument String firstNameFilter) {
 		
-		if (firstNameFilter == null) {
-			throw new BlossomException(null,BlossomExceptionType.VALIDATION,"1","this is a test exeption");
-		}
-		
 		return List.of(UsersMapper.convertEntitiyToDTO(userRepository.findById(1).get()));
+	}
+	
+	@DgsMutation
+	public User addUser(@InputArgument UserInput user) {
+		try
+		{
+			return UsersMapper.convertEntitiyToDTO(userRepository.save(UsersMapper.convertDTOtoEbtity(user)));
+		}
+		catch(DataIntegrityViolationException dataIntegrityViolationException)
+		{
+			throw new BlossomException(dataIntegrityViolationException,BlossomExceptionType.GENERAL,"ERR_INSERT_01", "User Was not Added, Verify The User is uniqe");
+		}
+					
+	
 	}
 }
--- src/main/java/com/blossom/users/mapper/UsersMapper.java
+++ src/main/java/com/blossom/users/mapper/UsersMapper.java
@@ -1,6 +1,7 @@
 package com.blossom.users.mapper;
 
 import com.blossom.usermanagmet.generated.types.User;
+import com.blossom.usermanagmet.generated.types.UserInput;
 import com.blossom.users.entities.Users;
 
 public class UsersMapper {
@@ -14,6 +15,36 @@ public class UsersMapper {
 						usersEntity.getAge(), 
 						usersEntity.getPreferredLanguage(), 
 						usersEntity.getEmail());
+		
+	}
+	
+	public static Users convertDTOtoEbtity(User user)
+	{
+		Users usersEntity  = new Users();
+		usersEntity.setFirstName(user.getFirstName());
+		usersEntity.setLastName(user.getLastName());
+		usersEntity.setMiddleName(user.getMiddleName());
+		usersEntity.setNickName(user.getNickName());
+		usersEntity.setAge(user.getAge());
+		usersEntity.setPreferredLanguage(user.getPreferredLanguage());
+		usersEntity.setEmail(user.getEmail());
+	
+		return usersEntity;
+		
+	}
+	
+	public static Users convertDTOtoEbtity(UserInput user)
+	{
+		Users usersEntity  = new Users();
+		usersEntity.setFirstName(user.getFirstName());
+		usersEntity.setLastName(user.getLastName());
+		usersEntity.setMiddleName(user.getMiddleName());
+		usersEntity.setNickName(user.getNickName());
+		usersEntity.setAge(user.getAge());
+		usersEntity.setPreferredLanguage(user.getPreferredLanguage());
+		usersEntity.setEmail(user.getEmail());
+	
+		return usersEntity;
 		
 	}
 
